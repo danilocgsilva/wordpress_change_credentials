067aaa8e1af7c31198636cd5193c393d4767f513
Danilo Carlos de Góes Silva <danilocgsilva>
2017-02-05 14:22:43 -0200

    Install sh improved

--- programfiles/install.sh
+++ programfiles/install.sh
@@ -2,3 +2,6 @@
 
 cp -f wpdbchapi /usr/local/bin
 cp -f wpdbnamech /usr/local/bin
+cp -f wpdbuserch /usr/local/bin
+cp -f wpdbpassch /usr/local/bin
+cp -f wpdbhostch /usr/local/bin
--- programfiles/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wpdbnamech wp-config.php asdf afsd
